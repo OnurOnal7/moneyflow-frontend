2ee0e58e7b7ef030abba69058e16c9299fff8be2
Erroll Barker <errollb@iastate.edu>
2024-04-04 17:38:11 -0500

    fixed main

--- Frontend/AndroidExample/app/src/main/java/com/example/androidexample/DefaultCardActivity.java
+++ Frontend/AndroidExample/app/src/main/java/com/example/androidexample/DefaultCardActivity.java
@@ -1,12 +1,12 @@
 package com.example.androidexample;
 
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
--- Frontend/AndroidExample/app/src/main/java/com/example/androidexample/FirstPageActivity.java
+++ Frontend/AndroidExample/app/src/main/java/com/example/androidexample/FirstPageActivity.java
@@ -1,10 +1,11 @@
 package com.example.androidexample;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 
 public class FirstPageActivity extends AppCompatActivity {
