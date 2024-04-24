0b4c1fa02fdcf485023e8273fdad96b2ddcd9199
Prakeerth Regunath <pkregu22@iastate.edu>
2024-04-23 20:17:22 -0500

    creating budgeting and updated goals

--- Frontend/AndroidExample/app/src/main/java/com/example/androidexample/GoalListActivity.java
+++ Frontend/AndroidExample/app/src/main/java/com/example/androidexample/GoalListActivity.java
@@ -1,11 +1,12 @@
 package com.example.androidexample;
+
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -24,22 +25,25 @@ public class GoalListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_checklist);
+        setContentView(R.layout.activity_goalview);
 
         listView = findViewById(R.id.listView);
         button = findViewById(R.id.addButton);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                addItem(v);
-            }
-        });
 
         items = new ArrayList<>();
         itemsadapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
         listView.setAdapter(itemsadapter);
         setUpListViewListener();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //startActivity(new Intent(GoalListActivity.this, GoalListAddActivity.class));
+
+            }
+        });
+
+
 
     }
 
@@ -53,27 +57,11 @@ public class GoalListActivity extends AppCompatActivity {
                 items.remove(position);
                 itemsadapter.notifyDataSetChanged();
                 return true;
-
             }
         });
     }
 
-    private void addItem(View view){
-        EditText input = findViewById(R.id.editText);
-        String itemtext = input.getText().toString();
 
-        if(!(itemtext.equals("")))
-        {
-            itemsadapter.add(itemtext);
-            input.setText("");
-        }
-        else {
-            Toast.makeText(getApplicationContext(), "Please Enter a Goal", Toast.LENGTH_LONG).show();
-        }
-
-
-
-    }
 
 
 
