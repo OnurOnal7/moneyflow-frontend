e16a27dc7c60a943a57799c7c4fde175c15a156d
Onur Onal <onur@iastate.edu>
2024-04-25 20:09:46 -0500

    Added documentation to family controller

--- Backend/MoneyFlow_Backend/src/main/java/ta4_1/MoneyFlow_Backend/Statements/StatementController.java
+++ Backend/MoneyFlow_Backend/src/main/java/ta4_1/MoneyFlow_Backend/Statements/StatementController.java
@@ -1,4 +1,18 @@
 package ta4_1.MoneyFlow_Backend.Statements;
 
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ta4_1.MoneyFlow_Backend.Users.User;
+import ta4_1.MoneyFlow_Backend.Users.UserRepository;
+
+/**
+ * Controller for Statements
+ *
+ * @author Onur Onal
+ * @author Kemal Yavuz
+ */
+
 public class StatementController {
 }
